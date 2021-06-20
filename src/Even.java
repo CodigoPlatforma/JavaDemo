cb5b941bfac192d7c5e4d676919f7f3608dbcae6
CodigoPlatforma <siliconcodingclub@gmail.com>
2021-06-20 11:38:31 +0530

    initialixation

---
+++ src/Even.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Even {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your range till you want prime: ");
+        int range = sc.nextInt();
+
+
+        for(int i =0;i<range;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+
+
+    }
+}
